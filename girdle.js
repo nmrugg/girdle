d32f13bd3bd83e6d74dac8e8fcb834e80fc61234
user <user@user.com>
2014-12-05 16:52:10 +0800

    Fixing formatting for cookies.js.

--- girdle.js
+++ girdle.js
@@ -779,10 +779,12 @@ var G = (function ()
         }());
         
         /*! Cookies.js - 0.3.1; Copyright (c) 2013, Scott Hamper; http://www.opensource.org/licenses/MIT */
+        /// https://github.com/ScottHamper/Cookies
         /// Streamed lined by Greenfield Education.
         (function (undefined)
         {
-            var Cookies = function (key, value, options) {
+            var Cookies = function (key, value, options)
+            {
                 return arguments.length === 1 ? Cookies.get(key) : Cookies.set(key, value, options);
             };
         
@@ -793,29 +795,33 @@ var G = (function ()
             };
             
             Cookies._document = document;
-        
-            Cookies.get = function (key) {
+            
+            Cookies.get = function (key)
+            {
                 if (Cookies._cachedDocumentCookie !== Cookies._document.cookie) {
                     Cookies._renewCache();
                 }
-        
+                
                 return Cookies._cache[key];
             };
-        
-            Cookies.set = function (key, value, options) {
+            
+            Cookies.set = function (key, value, options)
+            {
                 options = Cookies._getExtendedOptions(options);
                 options.expires = Cookies._getExpiresDate(value === undefined ? -1 : options.expires);
-        
+                
                 Cookies._document.cookie = Cookies._generateCookieString(key, value, options);
-        
+                
                 return Cookies;
             };
-        
-            Cookies.expire = function (key, options) {
+            
+            Cookies.expire = function (key, options)
+            {
                 return Cookies.set(key, undefined, options);
             };
-        
-            Cookies._getExtendedOptions = function (options) {
+            
+            Cookies._getExtendedOptions = function (options)
+            {
                 return {
                     path: options && options.path || Cookies.defaults.path,
                     domain: options && options.domain || Cookies.defaults.domain,
@@ -823,26 +829,29 @@ var G = (function ()
                     secure: options && options.secure !== undefined ?  options.secure : Cookies.defaults.secure
                 };
             };
-        
-            Cookies._isValidDate = function (date) {
+            
+            Cookies._isValidDate = function (date)
+            {
                 return Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date.getTime());
             };
-        
-            Cookies._getExpiresDate = function (expires, now) {
+            
+            Cookies._getExpiresDate = function (expires, now)
+            {
                 now = now || new Date();
                 switch (typeof expires) {
                     case "number": expires = new Date(now.getTime() + expires * 1000); break;
                     case "string": expires = new Date(expires); break;
                 }
-        
+                
                 if (expires && !Cookies._isValidDate(expires)) {
                     throw new Error("`expires` parameter cannot be converted to a valid Date instance");
                 }
-        
+                
                 return expires;
             };
-        
-            Cookies._generateCookieString = function (key, value, options) {
+            
+            Cookies._generateCookieString = function (key, value, options)
+            {
                 key = encodeURIComponent(key);
                 value = (value + "").replace(/[^!#$&-+\--:<-\[\]-~]/g, encodeURIComponent);
                 options = options || {};
@@ -852,44 +861,48 @@ var G = (function ()
                 cookieString += options.domain ? ";domain=" + options.domain : "";
                 cookieString += options.expires ? ";expires=" + options.expires.toUTCString() : "";
                 cookieString += options.secure ? ";secure" : "";
-        
+                
                 return cookieString;
             };
-        
-            Cookies._getCookieObjectFromString = function (documentCookie) {
+            
+            Cookies._getCookieObjectFromString = function (documentCookie)
+            {
                 var cookieObject = {};
                 var cookiesArray = documentCookie ? documentCookie.split("; ") : [];
-        
+                
                 for (var i = 0; i < cookiesArray.length; i++) {
                     var cookieKvp = Cookies._getKeyValuePairFromCookieString(cookiesArray[i]);
-        
+                    
                     if (cookieObject[cookieKvp.key] === undefined) {
                         cookieObject[cookieKvp.key] = cookieKvp.value;
                     }
                 }
-        
+                
                 return cookieObject;
             };
-        
-            Cookies._getKeyValuePairFromCookieString = function (cookieString) {
+            
+            Cookies._getKeyValuePairFromCookieString = function (cookieString)
+            {
                 /// "=" is a valid character in a cookie value according to RFC6265, so cannot `split("=")`
                 var separatorIndex = cookieString.indexOf("=");
-        
+                
                 /// IE omits the "=" when the cookie value is an empty string
                 separatorIndex = separatorIndex < 0 ? cookieString.length : separatorIndex;
-        
+                
                 return {
                     key: decodeURIComponent(cookieString.substr(0, separatorIndex)),
                     value: decodeURIComponent(cookieString.substr(separatorIndex + 1))
                 };
             };
-        
-            Cookies._renewCache = function () {
+            
+            Cookies._renewCache = function ()
+            {
                 Cookies._cache = Cookies._getCookieObjectFromString(Cookies._document.cookie);
                 Cookies._cachedDocumentCookie = Cookies._document.cookie;
             };
-        
-            Cookies._areEnabled = function () {
+            
+            Cookies._areEnabled = function ()
+            {
                 return Cookies.set("cookies.js", 1).get("cookies.js") === "1";
             };
             /// Removed enabled check. If you want to know, run Cookies._areEnabled().
